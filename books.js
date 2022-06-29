7fb9e1ae8de8b64585e0809f50f116aad0aedcad
Pranjal Bansal <78337467+Pranjal-code18@users.noreply.github.com>
2022-06-29 20:53:02 +0530

    Create books.js

---
+++ books.js
@@ -0,0 +1,19 @@
+export const books=[
+
+    {id:1,
+        author:"Jenny Han",
+         title:"We'll Always Have Summer : Mind training book for kids",
+        img:"https://images-eu.ssl-images-amazon.com/images/I/7175YpTSa7L._AC_UL302_SR302,200_.jpg",
+    },
+    
+    {id:2,
+        author:"Sudha Murty",
+         title:"Grandma's Bag of Stories: Collection of 20+ Stories",
+        img:"https://images-eu.ssl-images-amazon.com/images/I/81jv44QdNwL._AC_UL302_SR302,200_.jpg",
+    },
+    {id:3,
+        author:"Ruskin Bond",
+        title:"The Blue Umbrella : Book written by Ruskin Bond",
+       img:"https://images-eu.ssl-images-amazon.com/images/I/71e9MY-XE9L._AC_UL302_SR302,200_.jpg",
+    },
+    ];
